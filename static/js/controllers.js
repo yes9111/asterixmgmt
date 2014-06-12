746b8c68bd1e2dbb9fd13f9012a6baf2ec993f6e
Eugene Yang <eugeney@uci.edu>
2014-06-12 14:07:16 -0700

    Multiple PKs

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -107,10 +107,23 @@ asterface.controller('RowController', function($scope, $routeParams){
 });
 
 asterface.controller('NewDatasetController', function($scope){
-  $scope.createDataset = function()
+  $scope.datasetForm = {
+    primaryKeys: []
+  };
+  
+  $scope.datasetForm.addPrimaryKey = function(){
+    $scope.datasetForm.primaryKeys.push($scope.datasetForm.newPrimaryKey);
+  };
+  
+  $scope.datasetForm.removePK = function(pkIndex){
+    $scope.datasetForm.primaryKeys.splice(pkIndex, 1);
+  };
+  
+  $scope.datasetForm.createDataset = function()
   {
     var query = 'create dataset ' + $scope.datasetForm['name'] + ' (' + $scope.datasetForm['type'] + ') primary key ';
-    query += $scope.datasetForm.primarykey;
+    var pkString = $scope.datasetForm.primaryKeys.join(',');
+    query += pkString;
     
     A.dataverse($scope.browsing.dataverse).ddl(query, function(){
       alert("Successfully created dataset");
